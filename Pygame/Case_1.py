bae616f0ddf68faf2581e28324ca0f8305f7633e
dxd1dxd <dxdx1dxd@gmail.com>
2022-01-19 15:34:48 +0300

    Прорисовал фон, лицо и рот

--- Pygame/Case_1.py
+++ Pygame/Case_1.py
@@ -4,16 +4,14 @@ from pygame.draw import *
 pygame.init()
 
 FPS = 30
-screen = pygame.display.set_mode((400, 400))
-
-rect(screen, (255, 0, 255), (100, 100, 200, 200))
-rect(screen, (0, 0, 255), (100, 100, 200, 200), 5)
-polygon(screen, (255, 255, 0), [(100,100), (200,50),
-                               (300,100), (100,100)])
-polygon(screen, (0, 0, 255), [(100,100), (200,50),
-                               (300,100), (100,100)], 5)
-circle(screen, (0, 255, 0), (200, 175), 50)
-circle(screen, (255, 255, 255), (200, 175), 50, 5)
+screen = pygame.display.set_mode((300, 300))
+#coloring the background
+rect(screen, (210, 214, 255), (0, 0, 300, 300))
+#creating a face
+circle(screen, (255, 249, 48), (150, 150), 98)
+circle(screen, (0, 0, 0), (150, 150), 100, 2)
+#creating a mouth
+rect(screen, (0, 0, 0), (90, 200, 120, 20))
 
 pygame.display.update()
 clock = pygame.time.Clock()
