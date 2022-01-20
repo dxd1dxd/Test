79deeea1519cba9d6908387c1dfc6b7fef791ba4
dxd1dxd <dxdx1dxd@gmail.com>
2022-01-20 11:30:10 +0300

    Окончательная версия

--- Pygame/Case_1.py
+++ Pygame/Case_1.py
@@ -11,7 +11,20 @@ rect(screen, (210, 214, 255), (0, 0, 300, 300))
 circle(screen, (255, 249, 48), (150, 150), 98)
 circle(screen, (0, 0, 0), (150, 150), 100, 2)
 #creating a mouth
-rect(screen, (0, 0, 0), (90, 200, 120, 20))
+rect(screen, (0, 0, 0), (95, 200, 110, 20))
+#creating eyes
+circle(screen, ('#ff0000'), (100, 120), 20)
+circle(screen, (0, 0, 0), (100, 120), 22, 2)
+circle(screen, (0, 0, 0), (100, 120), 10)
+
+circle(screen, ('#ff0000'), (200, 120), 15)
+circle(screen, (0, 0, 0), (200, 120), 17, 2)
+circle(screen, (0, 0, 0), (200, 120), 7)
+
+#creating eyebrows
+polygon(screen, (0, 0, 0), [(50, 50), (40, 60), (130, 110), (140, 100)])
+polygon(screen, (0, 0, 0), [(170, 110), (160, 100), (230, 70), (240, 80)])
+
 
 pygame.display.update()
 clock = pygame.time.Clock()
